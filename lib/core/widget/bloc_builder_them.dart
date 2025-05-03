9b86db68b98a16077f4517b6ed6e021b7e67604a
Raafat <raafatm2001@gmail.com>
2025-05-03 05:56:57 +0300

    Refactor

--- lib/core/widget/bloc_builder_them.dart
+++ lib/core/widget/bloc_builder_them.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app_t/core/resources/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,15 +13,23 @@ class ThemToggleBuilder extends StatelessWidget {
     return BlocBuilder<ThemToggleCubit, ThemToggleState>(
       builder: (context, state) {
         final cubit = context.read<ThemToggleCubit>();
-        return IconButton(
-          onPressed: () {
-            cubit.toggleTheme();
-          },
-          icon: Icon(
-            cubit.isDark
-                ? Icons.toggle_on_outlined
-                : Icons.toggle_off_outlined,
-            size: 32,
+        return Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColor(context).blackColor,
+          ),
+          child: IconButton(
+            highlightColor: Colors.transparent,
+            onPressed: () {
+              cubit.toggleTheme();
+            },
+            icon: Icon(
+              cubit.isDark
+                  ? Icons.toggle_on_outlined
+                  : Icons.toggle_off_outlined,
+              size: 32,
+              color:AppColor(context).whiteColor,
+            ),
           ),
         );
       },
